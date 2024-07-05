ac6a1fc4cefb9f688bdb036e724f3327953fce07
LALIT KUSHWAHA <45065608+lalitkushwaha1410@users.noreply.github.com>
2024-07-06 01:01:33 +0530

    removed pause

--- cypress/e2e/feature1.cy.js
+++ cypress/e2e/feature1.cy.js
@@ -13,7 +13,6 @@ describe('feature-1 testing',()=>{
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').should('be.visible');
         cy.get('[data-test="login-button"]').click();
-        //cy.pause();
        
         //cy.get('.app_logo').should('be.visible').debug();
         cy.get('.app_logo').should('be.visible');
