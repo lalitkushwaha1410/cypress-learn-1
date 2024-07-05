0ac049c84a9b162c4152aeeec3295e66c7a0953d
LALIT KUSHWAHA <45065608+lalitkushwaha1410@users.noreply.github.com>
2024-07-06 00:03:09 +0530

    added yarn lock

--- cypress/e2e/feature1.cy.js
+++ cypress/e2e/feature1.cy.js
@@ -3,9 +3,31 @@
 describe('feature-1 testing',()=>{
 
     it.only('TestCase-1 for feature-1', ()=>{
-
-        
-
+        /* ==== Generated with Cypress Studio ==== */
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('.login_credentials_wrap-inner').click();
+        cy.get('[data-test="username"]').clear('standard_user');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="login-password"]').click();
+        cy.get('[data-test="password"]').clear('secret_sauce');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').should('be.visible');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('.app_logo').click();
+        cy.get('.app_logo').should('be.visible');
+        cy.get('[data-test="secondary-header"]').click();
+        cy.get('[data-test="title"]').should('be.visible');
+        cy.get('#react-burger-menu-btn').should('be.visible');
+        cy.get('#react-burger-menu-btn').click();
+        cy.get('[data-test="inventory-sidebar-link"]').should('have.text', 'All Items');
+        cy.get('[data-test="about-sidebar-link"]').should('have.text', 'About');
+        cy.get('[data-test="logout-sidebar-link"]').should('have.text', 'Logout');
+        cy.get('[data-test="reset-sidebar-link"]').should('have.text', 'Reset App State');
+        cy.get('#react-burger-cross-btn').click();
+        cy.get('[data-test="social-twitter"]').should('be.visible');
+        cy.get('[data-test="social-facebook"]').should('be.visible');
+        cy.get('[data-test="social-linkedin"]').should('be.visible');
+        /* ==== End Cypress Studio ==== */
     } )
 
 
